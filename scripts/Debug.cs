863fee36a0c6084692eb186248fb2eac32fcf8fe
itsung26 <iat2016@icloud.com>
2026-08-22 09:33:16 -0400

    initial project structuring

---
+++ scripts/Debug.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class Debug : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("Debug Quit"))
+		{
+			GetTree().Quit();
+		}
+	}
+
+	// Log a message to the console. Intended to replace Print().
+	public void Log<T>(T message)
+	{
+		GD.Print(message);
+	}
+}
