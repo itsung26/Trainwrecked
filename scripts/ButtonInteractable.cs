ecff020a44e03a29a46919222167a0619548901f
itsung26 <iat2016@icloud.com>
2026-08-25 16:45:58 -0400

    Update ButtonInteractable.cs

--- scripts/ButtonInteractable.cs
+++ scripts/ButtonInteractable.cs
@@ -4,11 +4,11 @@ using Godot.Collections;
 
 public partial class ButtonInteractable : RigidBody3D, IInteractable
 {
-	[Export] public String InteractableDisplayName;
-	[Export] public bool IsSelected;
-	[Export] public Array<MeshInstance3D> InvertedHullMeshes;
-	[Export] public bool OutlineVisible;
-	[Export] public bool CanBeSelected;
+	[Export] public string InteractableDisplayName { get; set; }
+	[Export] public bool IsSelected { get; set; }
+	[Export] public Array<MeshInstance3D> InvertedHullMeshes { get; set; } = new Array<MeshInstance3D>();
+	[Export] public bool OutlineVisible { get; set; }
+	[Export] public bool CanBeSelected { get; set; } = true;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
