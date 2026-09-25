f51d4258f4e53229ea7a5c5cc8d28c9b8eec1b47
itsung26 <iat2016@icloud.com>
2026-09-25 16:34:55 -0400

    system

--- scripts/TerrainChunk.cs
+++ scripts/TerrainChunk.cs
@@ -1,8 +1,71 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Represents a chunk of terrain, containing it's geometry, collision, and arraymesh plane.
+/// Not allowed to have a parent that is not of type TerrainGenerator.
+/// </summary>
 [Tool]
-// Represents a chunk of terrain, containing it's geometry, collision, and arraymesh plane.
 public partial class TerrainChunk : Node3D
 {
+	public override void _Ready()
+	{
+		// Prevent logic running when it is in tool in it's own scene.
+		if (Engine.IsEditorHint() && Owner == null)
+		{
+			return;
+		}
+	}
+
+	public void GenerateChunk(TerrainGenerator.LOD lod, Texture2D heightmap)
+	{
+        // Wipe away children first.
+        foreach (Node child in GetChildren())
+        {
+            if (child is not null)
+            {
+                child.Free();
+            }
+        }
+
+		switch (lod)
+		{
+			case TerrainGenerator.LOD.Full:
+                MeshInstance3D meshInstance = new MeshInstance3D();
+                AddChild(meshInstance);
+                meshInstance.Mesh = GenerateFlatPlane(32, 32);
+                meshInstance.Position = new Vector3(meshInstance.Position.X + 32 / 2, meshInstance.Position.Y, meshInstance.Position.Z + 32 / 2);
+				break;
+
+            case TerrainGenerator.LOD.Half:
+                break;
+
+            case TerrainGenerator.LOD.Quarter:
+                break;
+            
+            case TerrainGenerator.LOD.Skipdraw:
+                break;
+
+			default:
+				break;
+		}
+	}
+
+    /// <summary>
+    /// Returns an ArrayMesh corresponding to a flat plane with a subdivide
+    /// width and depth equal to resolution and a size equal to size.
+    /// </summary>
+    /// <returns></returns>
+    private ArrayMesh GenerateFlatPlane(int resolution, float size)
+    {
+        PlaneMesh basePlane = new PlaneMesh();
+        basePlane.SubdivideDepth = resolution;
+        basePlane.SubdivideWidth = resolution;
+        basePlane.Size = new Vector2(size, size);
+
+        ArrayMesh flatPlaneArrayMesh = new ArrayMesh();
+        flatPlaneArrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, basePlane.GetMeshArrays());
+
+        return flatPlaneArrayMesh;
+    }
 }
--- scripts/TerrainGenerator.cs
+++ scripts/TerrainGenerator.cs
@@ -6,57 +6,75 @@ using System;
 [GlobalClass]
 public partial class TerrainGenerator : Node3D
 {
+	public enum LOD
+	{
+		// Represents full vertex resolution.
+		Full = 0,
+		// Represents half vertex resolution.
+		Half = 1,
+		// Represents quarter vertex resolution.
+		Quarter = 2,
+		// Represents a no-draw.
+		Skipdraw = -1
+	}
+
 	/// <summary>
 	/// The camera that perceives the chunks. Chunk coordinate is derived from the
 	/// camera's position.
 	/// </summary>
 	[Export] public Camera3D Camera { get; set; }
 	[Export] public Texture2D BaseHeightmap { get; set; }
+	[Export] int FullLodResolution { get; set; }
 	/// <summary>
 	/// Length and width of each chunk.
 	/// </summary>
-	private float _chunkSize;
-	[Export]
-	public float ChunkSize
-	{
-		get => _chunkSize;
-		set
-		{
-			if (Mathf.IsEqualApprox(_chunkSize, value))
-			{
-				return;
-			}
-			_chunkSize = value;
-		}
-	}
+	[Export] public float ChunkSize { get; set; }
 	/// <summary>
 	/// The max ring distance in chunks where chunks will still be drawn.
 	/// </summary>
-	private int _maxDrawDistance;
-	[Export]
-	public int MaxDrawDistance
-	{
-		get => _maxDrawDistance;
-		set
-		{
-			if (_maxDrawDistance == value)
-			{
-				return;
-			}
-			_maxDrawDistance = value;
-		}
-	}
+	[Export] public int MaxDrawDistance { get; set; }
+
+
 	[ExportToolButton("Generate Terrain", Icon = "MeshInstance3D")]
 	public Callable GenerateTerrainButton => Callable.From(GenerateTerrain);
 
 
     public override void _Ready()
     {
+
     }
 
 	public void GenerateTerrain()
 	{
-		GD.Print("foo");
+		// Clear existing terrain first.
+		foreach (Node child in GetChildren())
+		{
+			if (child is not null)
+			{
+				child.Free();
+			}
+		}
+		GD.Print("Generating terrain...");
+		int chunksGenerated = 0;
+		while (chunksGenerated < GetChunkCount(MaxDrawDistance))
+		{
+			TerrainChunk newChunk = new TerrainChunk();
+			AddChild
+		}
+	}
+
+	/// <summary>
+	/// Returns the total amount of chunks that would fill
+	/// rings rings, including the center chunk.
+	/// </summary>
+	/// <returns></returns>
+	private int GetChunkCount(int rings)
+	{
+		TerrainChunk newChunk = new TerrainChunk();
+		AddChild
+		// Chebyshev radius R fills a square of side (2R + 1).
+		int side = (2 * rings) + 1;
+		return side * side;
 	}
 
 }
